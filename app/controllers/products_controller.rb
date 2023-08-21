f017daf8e5773e0e6b3cc598a629ac56370b934e
Armindo Matias <armindomatiasg@gmail.com>
2023-08-21 19:41:44 +0100

    changes in the route and pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,23 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[show]
 
-  def show
+  def index
+    @products = Product.all
+  end
 
+  def leisure
+    @products = Product.where(category: 'leisure')
+  end
+
+  def lunch
+    @products = Product.where(category: 'lunch')
+  end
+
+  def work
+    @products = Product.where(category: 'work')
+  end
+
+  def show
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  Product::CATEGORIES.each do |category|
+    get "products/#{category}", to: "products##{category}"
+  end
+
   resources :products
 end
