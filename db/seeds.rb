58e7c9205b9cd8f66f000a5d761f57da833bd479
Armindo Matias <armindomatiasg@gmail.com>
2023-08-22 20:42:43 +0100

    some changes on the show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-20.times do
+require "open-uri"
+require "json"
+
+# abrir o json
+json.parse("https://res.cloudinary.com/dtng1edik/image/list/upload.json")
+
+
+# para cada public_id ir buscar o nome do ficheiro
+
+# criar um produto com o nome do ficheiro
   Product.create(
     name: Faker::Superhero.name,
     category: Product::CATEGORIES.sample,
     description: Faker::Lorem.sentence(word_count: 15),
     price: rand(1..10),
-    user_id: 1,
+    user: User.first
   )
-end
+
+  file = URI.open(  "https://res.cloudinary.com/dtng1edik/image/upload/v1692722346/fotos%20airbnb/fotos%20-cl/#{}.heic")
+  article = Article.new(title: "NES", body: "A great console")
+  article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+  article.save
