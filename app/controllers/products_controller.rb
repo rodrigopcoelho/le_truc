71b0e5473623802611308fabee040c1695c04f46
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-08-22 11:36:50 +0100

    Ready to push

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,5 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all
   end
-
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
 class Product < ApplicationRecord
-  CATEGORIES = ["lunch", "work", "leisure"]
   belongs_to :user
-  validates :name, :description, :price, :category, presence: true
 end
