4f0d79fd9813a302920c8f8ebd4e187cf358173c
Luana Bernardino <luanabernardino2009@hotmail.com>
2023-08-22 18:00:11 +0100

    landing page basic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :landing
+
+  def landing
+  end
 
   def home
   end
 
-  def landing
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#landing"
+  devise_for :users
   resources :products
 end
