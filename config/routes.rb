0a9a91cd93cdd6b44446b534c787313f7c41aaf1
Margarida Almeida <margarida.almeida.92@gmail.com>
2023-08-22 15:17:55 +0100

    More changes on index css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-<<<<<<< HEAD
-
-=======
-  # tirar se der conflito
->>>>>>> products-index
   resources :products
 end
