2ee6371e74663ec5ce2bf51f2892f011ec59ec43
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-08-23 12:32:56 +0100

    routes and login and pages done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#landing"
   devise_for :users
+  get '/homepage' => "products#index", :as => :user_root
   resources :products
   get "dashboard", to: "products#dashboard"
 end
