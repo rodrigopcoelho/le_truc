6d37e269e79eac36b955520e1a988322e0b8554f
Armindo Matias <armindomatiasg@gmail.com>
2023-08-24 12:55:11 +0100

    controller booking logic to add/remove tokens

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,47 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[destroy]
+  before_action :set_tokens_user, :set_product_price, only: %i[create destroy]
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user.id
+    # tokens_user = current_user.tokens_balance
+    # product_price = @booking.slot.product.price
+
+    if tokens_user >= product_price
+      @booking.save
+      @tokens_user -= product_price
+      # the below line is needed to save the new balance tokens in the user?
+      current_user.save
+    else
+      flash.alert("You need #{product_price - tokens_user} to make the booking")
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    if @booking.destroy
+      @tokens_user += @product_price
+    else
+      flash.alert("There is a problem with your request, try again later.")
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:slot_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_tokens_user
+    @tokens_user = current_user.tokens_balance
+  end
+
+  def set_product_price
+    @product_price = @booking.slot.product.price
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :products
   has_many :bookings
+
+  validates tokens_balance 
 end
