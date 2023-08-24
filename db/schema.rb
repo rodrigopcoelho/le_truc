0079ddfde3cb389d844cc1dbdf9f23538422aa92
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-08-24 11:32:07 +0100

    database and stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_154638) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_103111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_154638) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "slot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slot_id"], name: "index_bookings_on_slot_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "category"
@@ -51,6 +60,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_154638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "slots", force: :cascade do |t|
+    t.integer "start_time"
+    t.integer "end_time"
+    t.boolean "availability"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_slots_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,5 +88,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_154638) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "slots"
+  add_foreign_key "bookings", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "slots", "products"
 end
