5e61bc1fdb6b04a17ca20492575639fde97488c4
Armindo Matias <armindomatiasg@gmail.com>
2023-08-24 12:00:53 +0100

    add column tokens balance to the model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_154638) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_102225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_154638) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.boolean "admin", default: false
+    t.integer "tokens_balance", default: 5
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
